784085897c8c6c1c946b614e3aaf0914493b34cf
inier <inier@qq.com>
2020-02-18 00:45:21 +0800

    fix: 更新

--- scripts/units/FriendlyErrorsWebpackPlugin.js
+++ scripts/units/FriendlyErrorsWebpackPlugin.js
@@ -1,10 +1,16 @@
 // 友好错误提示插件
 // https://github.com/geowarin/friendly-errors-webpack-plugin
 
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+// const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
-module.exports = ({config}) => {
+module.exports = ({ config }) => {
+    const { transformer, formatter } = require('../../lib/resolveLoaderError');
     return () => {
-        config.plugin('error').use(FriendlyErrorsWebpackPlugin);
+        config.plugin('friendly-errors').use(require('@soda/friendly-errors-webpack-plugin'), [
+            {
+                additionalTransformers: [transformer],
+                additionalFormatters: [formatter],
+            },
+        ]);
     };
 };
