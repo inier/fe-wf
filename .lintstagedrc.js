674da14e4f011f395da3fdd0414f84cb8fd8b4de
inier <inier@qq.com>
2020-02-18 00:55:38 +0800

    fix: 添加 tslint 并开启自动修复

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,5 +5,5 @@
 module.exports = {
     '*.{vue,htm,html,css,sss,less,scss}': ['prettier --write', 'lint stylelint', 'git add'],
     '*.{js,jsx}': ['prettier --write', 'box lint eslint', 'git add'],
-    'src/**/*.{ts,tsx}': ['prettier --parser typescript --write', 'box lint eslint', 'git add'],
+    '*.{ts,tsx}': ['prettier --parser typescript --write', 'box lint tslint', 'git add'],
 };
